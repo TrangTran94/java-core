02de3c49c190f6a41ffeeb21e8d2e88d3117b395
Marco <marco.bui.dev@gmail.com>
2024-12-19 21:05:53 +0700

    Add file.

---
+++ HerrietakoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerrietakoApp.swift
+//  Herrietako
+//
+//  Created by Marco on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HerrietakoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
